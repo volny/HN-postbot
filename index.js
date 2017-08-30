97c16efd4a14857cefb4b1ad9d00139b7308c8dd
Felix Volny <felix@volny.co>
2017-08-30 22:20:20 +0700

    scrape element content

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
+const secrets = require('./secrets.js')
 
 const URI = 'https://news.ycombinator.com/login'
-const USERNAME_SELECTOR = 'body > div > form:nth-child(4) > table > tbody > tr:nth-child(1) > td:nth-child(2) > input[type="text"]'
-const PASSWORD_SELECTOR = 'body > div > form:nth-child(4) > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type="password"]'
-const BUTTON_SELECTOR = 'body > div > form:nth-child(4) > input[type="submit"]'
 
-const login = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+// TODO these selectors for not unique on the page, the signup form below uses the same.
+// currently only works because we are returned the first match, which happens to be what we want
+const USERNAME_SELECTOR = 'input[type="text"][name="acct"]'
+const PASSWORD_SELECTOR = 'input[type="password"][name="pw"]'
+const BUTTON_SELECTOR = 'input[type="submit"][value="login"]'
 
-  browser.close();
+const login = async (page) => {
+  try {
+    await page.click(USERNAME_SELECTOR)
+    await page.type(secrets.username)
+    await page.click(PASSWORD_SELECTOR)
+    await page.type(secrets.password)
+    await page.click(BUTTON_SELECTOR)
+    await page.waitForNavigation()
+
+  } catch (error) {
+    console.error(error)
+  }
 }
 
+const getInnerHTML = async (page, selector) => {
+  try {
+    return await page.evaluate((selector) => {
+      const element = document.querySelector(selector)
+      return element ? element.innerHTML : null
+    }, selector)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+;(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(URI)
+
+  await login(page)
+
+  await page.goto(`https://news.ycombinator.com/user?id=${secrets.username}`)
+  const createdAtSelector = '#hnmain > tbody > tr:nth-child(3) > td > form > table > tbody > tr:nth-child(2) > td:nth-child(2)'
+  const createdAt = await getInnerHTML(page, createdAtSelector)
+  console.log(createdAt)
+
+  browser.close()
+})()
