e3b69277a51efd3002a63bb3032d5a77df6f3f92
Felix Volny <felix@volny.co>
2017-09-01 00:50:06 +0700

    links and twitterIDs with fugly method

--- techmeme.js
+++ techmeme.js
@@ -7,35 +7,45 @@ const URI = 'https://www.techmeme.com/river'
 
 // const twitterURI = `https://twitter.com/intent/retweet?tweet_id=${twitterID}&related=mediagazer`
 
-const getStories = async (page) => {
-  try {
-    return await page.evaluate(() => {
-      const nodelist = document.querySelectorAll('#countercol > table > tbody > tr')
-      return Array.from(nodelist)
-    })
-  } catch (error) {
-    console.error(error)
-  }
+// I dislike that I have to traverse the DOM twice to get my links/twitter ids
+// the 'correct' way of doing this would be to `getStories`, an array of DOM nodes, and then get whatever out of that
+// trouble is chrome gives me an array of 'elementHandles' that have no (documented) way to access it's children
+// see commit 'error - why do we get empty responses' for the approach I took
+
+const getLinks = async (page) => {
+  const links = await page.evaluate(() => {
+    const tags = [...document.querySelectorAll('#countercol > table > tbody > tr > td > a')]
+    return tags.map((tag) => tag.getAttribute('href'))
+  })
+  return links
 }
 
-const getLink = story => story.querySelector('td > a').getAttribute('href')
-const getTwitterID = story => story.querySelector('td > .rshr').getAttribute('twid')
+const getTwitterIDs = async (page) => {
+  const twitterIDs = await page.evaluate(() => {
+    const tags = [...document.querySelectorAll('#countercol > table > tbody > tr > td > .rshr')]
+    return tags.map((tag) => tag.getAttribute('twid'))
+  })
+  return twitterIDs
+}
+
+const makeObject = (links, twitterIDs) => (
+  links.map((link, i) => ({
+    link: link,
+    twitterID: twitterIDs[i]
+  }))
+)
 
 ;(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto(URI)
 
-  const stories = await getStories(page)
+  const links = await getLinks(page)
+  const twitterIDs = await getTwitterIDs(page)
 
-  const data = stories.map((story) => {
-    return {
-      link: getLink(story),
-      twitterID: getTwitterID(story)
-    }
-  })
+  const linksWithIDs = makeObject(links, twitterIDs)
 
-  console.log(data)
+  console.log(linksWithIDs)
 
   browser.close()
 })()
