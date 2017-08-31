77e72db00fa802179d896a00e45da3e31adb2696
Felix Volny <felix@volny.co>
2017-09-01 02:41:01 +0700

    wrap it all in try/catch

--- techmeme.js
+++ techmeme.js
@@ -8,37 +8,47 @@ const URI = 'https://www.techmeme.com/river'
 // see commit 'error - why do we get empty responses' for the approach I took
 
 const getLinks = async (page) => {
-  const links = await page.evaluate(() => {
-    const tbodies = document.querySelectorAll('#countercol > table > tbody')
-    const today = [...tbodies[0].querySelectorAll('.ritem > td > a')]
-    return today.map((story) => story.getAttribute('href'))
-  })
-  return links
+  try {
+    const links = await page.evaluate(() => {
+      const tbodies = document.querySelectorAll('#countercol > table > tbody')
+      const today = [...tbodies[0].querySelectorAll('.ritem > td > a')]
+      return today.map((story) => story.getAttribute('href'))
+    })
+    return links
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 const getTwitterIDs = async (page) => {
-  const twitterIDs = await page.evaluate(() => {
-    const tbodies = document.querySelectorAll('#countercol > table > tbody')
-    const today = [...tbodies[0].querySelectorAll('.ritem > td > .rshr')]
-    return today.map((story) => story.getAttribute('twid'))
-  })
-  return twitterIDs
+  try {
+    const twitterIDs = await page.evaluate(() => {
+      const tbodies = document.querySelectorAll('#countercol > table > tbody')
+      const today = [...tbodies[0].querySelectorAll('.ritem > td > .rshr')]
+      return today.map((story) => story.getAttribute('twid'))
+    })
+    return twitterIDs
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 const getTitleFromID = async (page, twitterID) => {
-  // navigate to retweet page
   const twitterURI = `https://twitter.com/intent/retweet?tweet_id=${twitterID}&related=mediagazer`
   // techmeme has this bug where sometimes it creates no twitterlink
   // e.g. https://www.techmeme.com/170831/p11#a170831p11
   if (twitterID) {
-    await page.goto(twitterURI)
-    // get tweet text and extract short title
-    const title = await page.evaluate(() => {
-      const text = document.querySelector('.tweet-text').innerHTML
-      const title = text.substring(0, text.indexOf('<a') - 1)
+    try {
+      await page.goto(twitterURI)
+      const title = await page.evaluate(() => {
+        const text = document.querySelector('.tweet-text').innerHTML
+        const title = text.substring(0, text.indexOf('<a') - 1)
+        return title
+      })
       return title
-    })
-    return title
+    } catch (error) {
+      console.error(error)
+    }
   } else {
     return ''
   }
@@ -47,9 +57,13 @@ const getTitleFromID = async (page, twitterID) => {
 const makeSubmissionList = async (page, links, twitterIDs) => {
   // not proud, but `map` doesn't `await`, and this is the most straightforward and readable way
   let titles = []
-  for (let twitterID of twitterIDs) {
-    const title = await getTitleFromID(page, twitterID)
-    titles.push(title)
+  try {
+    for (let twitterID of twitterIDs) {
+      const title = await getTitleFromID(page, twitterID)
+      titles.push(title)
+    }
+  } catch (error) {
+    console.error(error)
   }
 
   return links.map((link, i) => ({
@@ -63,12 +77,14 @@ const makeSubmissionList = async (page, links, twitterIDs) => {
   const page = await browser.newPage()
   await page.goto(URI)
 
-  const links = await getLinks(page)
-  const twitterIDs = await getTwitterIDs(page)
-
-  const submissionList = await makeSubmissionList(page, links, twitterIDs)
-
-  console.log(submissionList)
+  try {
+    const links = await getLinks(page)
+    const twitterIDs = await getTwitterIDs(page)
+    const submissionList = await makeSubmissionList(page, links, twitterIDs)
+    console.log(submissionList)
+  } catch (error) {
+    console.error(error)
+  }
 
   browser.close()
 })()
