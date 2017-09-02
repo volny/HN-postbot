9220637ec0765829f5b52ffed20c954c96dae72b
Felix Volny <felix@volny.co>
2017-09-02 12:01:06 +0700

    check if posting succeeded

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-// TODO before production:
-// we need 'moderation' - can't post all items at the speed of node
-// (1) the cron job to execute this should run every random number of minutes between 30 - 90
-// (2) after posting a story we should sleep for 1-3 minutes before posting the next
+const DEBUG = true
 
 const puppeteer = require('puppeteer')
 const login = require('./hnLogin').login
 const getStories = require('./techmeme').getStories
 const filterPosted = require('./algolia').filterPosted
 const postStory = require('./hnPost.js').postStory
+const postHasSucceeded = require('./confirm.js').postHasSucceeded
 
 ;(async () => {
   try {
-    const browser = await puppeteer.launch({headless: false})
+    const browser = await puppeteer.launch({headless: !DEBUG})
     const page = await browser.newPage()
 
     // fetch the stories object from techmeme
@@ -20,10 +18,11 @@ const postStory = require('./hnPost.js').postStory
     const unpostedStories = await filterPosted(submissionsList)
     // login to hn
     await login(page)
-    // post stories
-    post all stories
+    // post stories and check if post succeeded
     for (let story of unpostedStories) {
       await postStory(page, story)
+      const hasSucceeded = await postHasSucceeded(story.link)
+      console.log(hasSucceeded ? 'Success:' : 'Failure:', story.title)
     }
 
     browser.close()
